d767f5b6caba5b243e42d86fcc1a4418f96ab8b6
yangjing <yangjingpuyu@aliyun.com>
2018-08-03 11:18:42 +0800

    new searchcode

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="toast-info right-lv">
-    <div class="box-s"></div>
+  <div class="toast-info" :class="{'left': direction === 'left', 'right': direction === 'right'}">
+    <div class="box-s" :class="{'left': direction === 'right', 'right': direction === 'left'}"></div>
     <img class="img-info" src="../assets/default.png"/>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    direction: {
+      type: String,
+      default: 'left'
+    }
+  }
 }
 </script>
 
@@ -21,17 +27,24 @@ export default {
     -webkit-box-shadow: 0 0px 20px #ead8d8
     border-radius: 5px
     z-index: 100
-    &.right-lv
+    &.right
+      left: 140px
+    &.left
       right: 140px
     .box-s
       position: absolute
-      right: -10px
       top: 7px
       width: 0
       height: 0
       border-width: 5px
       border-style: solid
-      border-color: transparent transparent transparent #fff
+
+      &.right
+        right: -10px
+        border-color: transparent transparent transparent #fff
+      &.left
+        left: -10px
+        border-color: transparent #fff transparent transparent
     .img-info
       width: 180px
       height: 180px
--- src/views/searchCode.vue
+++ src/views/searchCode.vue
@@ -20,8 +20,63 @@
         <matchingParts/>
         <div class="search-resule">
           <div class="search-title">
-            
+            <h3>GT1号</h3>
+            <h3>配件类型</h3>
           </div>
+          <ul class="search-th">
+            <li class="search-td">
+              <div class="search-gt1 search-box black">
+                <h3>
+                  <a href="javascript:;">
+                    <span>TYO-1342352</span>
+                    <Toast :direction="'right'"></Toast>
+                  </a>
+                </h3>
+                <h3>机油滤清器</h3>
+              </div>
+            </li>
+            <li class="search-td">
+              <div class="search-gt1 search-box black">
+                <h3>
+                  <a href="javascript:;">
+                    <span>TYO-1342352</span>
+                    <Toast :direction="'right'"></Toast>
+                  </a>
+                </h3>
+                <h3>机油滤清器</h3>
+              </div>
+            </li>
+            <li class="search-td">
+              <div class="search-gt1 search-box black">
+                <h3>
+                  <a href="javascript:;">
+                    <span>TYO-1342352</span>
+                    <Toast :direction="'right'"></Toast>
+                  </a>
+                </h3>
+                <h3>机油滤清器</h3>
+              </div>
+            </li>
+            <li class="search-td">
+              <div class="search-gt1 search-box black white">
+                <h3>
+                  <a href="javascript:;">
+                    <span>TYO-1342352</span>
+                    <Toast :direction="'right'"></Toast>
+                  </a>
+                </h3>
+                <h3>机油滤清器</h3>
+              </div>
+              <div class="search-other search-box">
+                <h3>对标型号</h3>
+                <ul class="other">
+                  <li><h3>TYO-1342352</h3></li>
+                  <li><h3>TYO-1342352</h3></li>
+                  <li><h3>TYO-1342352</h3></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -32,12 +87,13 @@
 // @ is an alias to /src
 import searchView from '@/components/searchView'
 import matchingParts from '@/components/matchingParts'
-
+import Toast from '@/components/toast'
 export default {
   name: 'home',
   components: {
     searchView,
-    matchingParts
+    matchingParts,
+    Toast
   },
   data () {
     return {
@@ -46,6 +102,8 @@ export default {
 }
 </script>
 <style scoped lang="stylus">
+  ul
+    list-style: none
   .home-container
     width: 100%
     overflow: hidden
@@ -89,15 +147,76 @@ export default {
       .banner-content
         width: 100%
         overflow: hidden
-        margin-bottom: 100px
+        padding-bottom: 150px
         .search-resule
           width: 855px
-          height: 1000px
           float: right
-          background-color: #f2f2f2
           .search-title
+            display: flex
             width: 100%
-            height: 1000px
+            height: 40px
+            box-sizing: border-box
+            padding-left: 30px
+            background-color: #727172
+            line-height: 40px
+            & > h3:nth-child(1)
+              width: 254px
+              font-size: 16px
+              color: #fff
+            &  > h3:nth-child(2)
+              flex: 1
+              font-size: 16px
+              color: #fff
+          .search-th
+            width: 100%
+            .search-td
+              width: 100%
+              .search-box
+                display: flex
+                box-sizing: border-box
+                width: 100%
+                min-height: 40px
+                padding-left: 30px
+                line-height: 40px
+
+                &.black
+                  color: #646464
+                &.white
+                  background-color: #f2f2f2
+              .search-gt1
+                a
+                  position: relative
+                  font-size: 16px
+                  color: #646464
+                  text-decoration: none
+                  &:hover
+                    text-decoration: underline
+                    .toast-info
+                      display: block
+              .search-gt1 > h3:nth-child(1)
+                width: 254px
+                font-size: 16px
+              .search-gt1 > h3:nth-child(2)
+                flex: 1
+                font-size: 16px
+              .search-other
+                background-color: #b5b5b6
+                color: #fff
+              .search-other > h3
+                width: 100px
+                font-size: 16px
+              .other
+                flex: 1
+                min-height: 40px
+                & > li
+                  display: inline-block
+                  box-sizing: border-box
+                  padding: 5px 0px
+                  width: 160px
+                  height: 30px
+                  line-height: 30px
+                  & > h3
+                    font-size: 16px
       .hot-parts
         width: 100%
         overflow: hidden
